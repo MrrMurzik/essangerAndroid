b71ca8ecf3a7aecb23671269f7c6a8ef9383537f
MrrMurzik <s.vasilyev97@gmail.com>
2023-01-02 20:25:27 +0200

    first commit

---
+++ app/src/main/java/com/example/messengerAndroid/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.messengerAndroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.messengerAndroid.databinding.MyProfileBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: MyProfileBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = MyProfileBinding.inflate(layoutInflater)
+        binding.tvName.text = intent.getStringExtra("name")
+        setContentView(binding.root)
+    }
+}
