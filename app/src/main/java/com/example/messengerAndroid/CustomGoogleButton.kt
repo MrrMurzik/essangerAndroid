0323f323da99bce554660e8b745a0db815b4ec50
MrrMurzik <s.vasilyev97@gmail.com>
2023-02-08 11:25:16 +0200

    finished button

--- app/src/main/java/com/example/messengerAndroid/CustomGoogleButton.kt
+++ app/src/main/java/com/example/messengerAndroid/CustomGoogleButton.kt
@@ -1,103 +1,123 @@
 package com.example.messengerAndroid
 
-import android.annotation.SuppressLint
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.RectF
-import android.graphics.Typeface
+import android.graphics.*
 import android.util.AttributeSet
-import android.util.Log
 import android.util.TypedValue
-import android.view.LayoutInflater
 import android.view.View
-import android.widget.LinearLayout
-import androidx.core.graphics.toColor
+import androidx.core.content.res.ResourcesCompat
 import androidx.vectordrawable.graphics.drawable.VectorDrawableCompat
-import com.example.messengerAndroid.databinding.CustomGoogleButtonBinding
 
 
-private const val RADIUS = 10f
+private const val RADIUS = 20f
+private const val MIN_HEIGHT_DP = 40f
+private const val ICON_SIZE = 20f
+private const val ICON_TEXT_INTERVAL = 48f
+private const val TEXT_SIZE = 18f
 
 class CustomGoogleButton @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
 
+    private val iconSize = convertDpToPx(ICON_SIZE)
+    private val fontFamily = ResourcesCompat.getFont(context, R.font.roboto_medium)
+    private var textStartX = 0f
+    private var textStartY = 0f
+    private val textHeight = convertSpToPx(TEXT_SIZE)
+
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.FILL
         textAlign = Paint.Align.CENTER
-        typeface = Typeface.create("open_sans", Typeface.NORMAL)
+        typeface = fontFamily
     }
 
-    private val googleIconDrawable = VectorDrawableCompat.create(resources, R.drawable.ic_google_logo, null)
+    private val googleIconDrawable = VectorDrawableCompat.create(
+        resources,
+        R.drawable.ic_google_logo,
+        null)
 
-    private val rect = RectF(0f, 0f, 0f, 0f)
-
-    init {
-        isClickable = true
-        calculateRect()
-    }
-
-    @SuppressLint("ResourceAsColor")
-    override fun onDraw(canvas: Canvas) {
-        super.onDraw(canvas)
-        paint.color = Color.WHITE
-        canvas.drawRoundRect(rect, RADIUS, RADIUS, paint)
-
-        paint.color = Color.BLACK
-        canvas.drawText("GOOGLE", width * 0.52f, height * 0.66f, paint)
-
-        googleIconDrawable?.draw(canvas)
-    }
-
+    private val buttonRect = RectF(0f, 0f, 0f, 0f)
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-    }
+        val widthMode = MeasureSpec.getMode(widthMeasureSpec)
+        val heightMode = MeasureSpec.getMode(heightMeasureSpec)
 
-
-    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
-        super.onSizeChanged(w, h, oldw, oldh)
-        calculateRect()
-        calculateDrawablePos()
-        paint.textSize = height / 2f
-
-    }
-
-    private fun calculateDrawablePos() {
-        var top = (height * 0.25).toInt()
-        var bottom = (height * 0.75).toInt()
-        var left = (width * 0.33).toInt()
-        var right = (width * 0.39).toInt()
-
-        val topBottomDiagonal = bottom - top
-        val leftRightDiagonal = right - left
-
-        if (topBottomDiagonal > leftRightDiagonal) {
-            val excessOfBiggerDiagonal = topBottomDiagonal - leftRightDiagonal
-            top += excessOfBiggerDiagonal / 2
-            bottom -= excessOfBiggerDiagonal / 2
-        } else if (topBottomDiagonal < leftRightDiagonal) {
-            val excessOfBiggerDiagonal = topBottomDiagonal - leftRightDiagonal
-            left += excessOfBiggerDiagonal / 2
-            right -= excessOfBiggerDiagonal / 2
+        val width = when (widthMode) {
+            MeasureSpec.AT_MOST -> {
+                MeasureSpec.getSize(widthMeasureSpec) / 3
+            }
+            else -> {
+                widthMeasureSpec
+            }
+        }
+        val height = when (heightMode) {
+            MeasureSpec.AT_MOST -> {
+                convertDpToPx(MIN_HEIGHT_DP)
+            }
+            else -> {
+                heightMeasureSpec
+            }
         }
 
-        googleIconDrawable?.setBounds(left, top, right, bottom)
+        setMeasuredDimension(width, height)
     }
 
-    private fun calculateRect() {
-        rect.left = 0f
-        rect.right = width.toFloat()
-        rect.bottom = height.toFloat()
-        rect.top = 0f
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+
+        super.onSizeChanged(w, h, oldw, oldh)
+        calculateButtonPos()
+        calculateContentPos()
+        paint.textSize = textHeight.toFloat()
+
     }
 
-    private fun convertDpToPx(dp: Float) : Float {
+    private fun calculateButtonPos() {
+        buttonRect.left = 0f
+        buttonRect.right = width.toFloat()
+        buttonRect.bottom = height.toFloat()
+        buttonRect.top = 0f
+    }
+
+    private fun calculateContentPos() {
+        val iconTopPos = (height - iconSize) / 2
+        val iconBottomPos = iconSize + iconTopPos
+
+        val iconLeftPos = (width * 0.33).toInt()
+        val iconRightPos = iconLeftPos + iconSize
+
+        googleIconDrawable?.setBounds(iconLeftPos, iconTopPos, iconRightPos, iconBottomPos)
+
+        calculateTextStartPos(iconRightPos)
+    }
+
+    private fun calculateTextStartPos(iconRightPos: Int) {
+        textStartX = iconRightPos + convertDpToPx(ICON_TEXT_INTERVAL).toFloat()
+        textStartY = height / 2 + textHeight / 2.5f
+    }
+
+
+    override fun onDraw(canvas: Canvas) {
+
+        paint.color = Color.WHITE
+        canvas.drawRoundRect(buttonRect, RADIUS, RADIUS, paint)
+
+        paint.color = Color.BLACK
+        canvas.drawText("GOOGLE", textStartX, textStartY, paint)
+
+        googleIconDrawable?.draw(canvas)
+
+    }
+
+
+    private fun convertDpToPx(dp: Float) : Int {
         return TypedValue.applyDimension(
             TypedValue.COMPLEX_UNIT_DIP, dp, context.resources.displayMetrics
-        )
+        ).toInt()
     }
 
+    private fun convertSpToPx(sp: Float) : Int {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_SP, sp, context.resources.displayMetrics
+        ).toInt()
+    }
 }
