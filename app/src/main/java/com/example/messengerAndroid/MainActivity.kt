a865e5c47606917f49e489284337e248da66331b
MrrMurzik <s.vasilyev97@gmail.com>
2023-02-08 11:48:48 +0200

    Done my_profile :q

--- app/src/main/java/com/example/messengerAndroid/MainActivity.kt
+++ app/src/main/java/com/example/messengerAndroid/MainActivity.kt
@@ -10,28 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = MyProfileBinding.inflate(layoutInflater)
-        binding.tvName.text = intent.getStringExtra("name")
+        binding.textViewName?.text = intent.getStringExtra("name")
         setContentView(binding.root)
     }
 }
-
-
-//<com.google.android.material.button.MaterialButton
-//android:id="@+id/btnGoogle"
-//android:layout_width="match_parent"
-//android:layout_height="wrap_content"
-//app:layout_constraintStart_toStartOf="parent"
-//app:layout_constraintEnd_toEndOf="parent"
-//app:layout_constraintBottom_toTopOf="@id/tvOr"
-//app:layout_constraintTop_toBottomOf="@id/etPassword"
-//app:layout_constraintVertical_chainStyle="packed"
-//app:layout_constraintVertical_bias="1"
-//android:background="@drawable/slightly_rounded_corner"
-//app:backgroundTint="@color/white"
-//android:text="@string/google"
-//app:icon="@drawable/ic_google_logo"
-//android:textColor="@color/black"
-//app:iconTint="@null"
-//app:iconGravity="textStart"
-//app:iconPadding="@dimen/google_icon_padding"
-///>
